7279a8d2b2ee5ebe3eb143ebff1bde9fc50774b1
Ricard Mallafre <rikitzzz@gmail.com>
2025-01-10 13:27:38 +0100

    feat(main): use react and typescript

---
+++ src/app.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+
+ReactDOM.createRoot(document.body).render(
+  <React.StrictMode>
+    <h1>Hello, World!</h1>
+  </React.StrictMode>,
+);
